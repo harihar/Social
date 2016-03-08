1a18a65f4e09dbd3237fe2e2cc26baf32e3f963c
Harihar <harihar.246@gmail.com>
2016-03-08 10:15:21 +0530

    added an endpoint to show version no of the app

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var express = require("express");
 var cool = require('cool-ascii-faces');
+var pjson = require('./package.json');
 
 var app = express();
+
+app.get("/", function (req, res) {
+    res.send("Social v" + pjson.version);
+});
 
 app.get("/hello", function (req, res) {
     res.send("Hello world");
